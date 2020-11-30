650e822ebf38a7c2cdad06288c92b0704343b6f8
chenzhenshen <1915478358@qq.com>
2020-11-30 20:34:28 +0800

    第一次提交

---
+++ nnxyczs/src/com/nnxy/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.nnxy.controller;
+
+public class UserController {
+	private String name;
+	private int id;
+
+}
---
+++ nnxyczs/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author czs
+ *
+ */
+module nnxyczs {
+}
